420d790d52ae47e0c27e9c2dd0042adcf862140c
mynameishamish <hamish.tennent@gmail.com>
2017-09-29 11:23:18 -0400

    Remove Slack Token

--- slackpost.py
+++ slackpost.py
@@ -1,11 +1,11 @@
 from slackclient import SlackClient
 
-slack_token = "xoxp-110015888069-110992073120-249898224518-c2ebe2ef2b985d2f0e28f552d75658cc"
+slack_token = ""
 sc = SlackClient(slack_token)
 
 sc.api_call(
   "chat.postMessage",
   channel="#scripting",
   print(users.info)
-  # text="Spyderbot Lives! :tada:"
+  text="Spyderbot Lives! :tada:"
 )
