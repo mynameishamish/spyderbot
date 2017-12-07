2d0eafce40dd6dfbdb87b85044e2d1117e7c7f96
mynameishamish <hamish.tennent@gmail.com>
2017-12-06 21:16:36 -0500

    Updated Readme and motions/rest.py

--- motions.py
+++ motions.py
@@ -33,14 +33,6 @@ spyder_config = {
 import time
 import numpy
 import pypot.robot
-import motions
-
-from Adafruit_Thermal import *
-
-printer = Adafruit_Thermal("/dev/ttyUSB0", 19200, timeout=99999999)
-printer.begin()
-printer.feed(-5)
-
 
 amp = 30
 freq = 0.5
@@ -54,36 +46,35 @@ for m in robot.motors: # Note that we always provide an alias for all motors.
     # m.goal_position = 0
 
 # Wait for the robot to actually reach the base position.
-time.sleep(2)
 # pos = 0
 
 
-rest(10,10,10)
+# Position definitions, just pass in moving_speed for each motor
 
-time.sleep(5)
+def rest(z, x, c):
+    robot.m1.moving_speed = z
+    robot.m2.moving_speed = x
+    robot.m3.moving_speed = c
+    robot.m1.goal_position = 0
+    robot.m2.goal_position = 20.3
+    robot.m3.goal_position = -6
+    print("Rest");
 
-robot.m1.goal_position = -70
-robot.m2.goal_position = 15.5
-robot.m3.goal_position = 23.3
-print("Alert")
+def alert(z, x, c):
+    robot.m1.moving_speed = z
+    robot.m2.moving_speed = x
+    robot.m3.moving_speed = c
+    robot.m1.goal_position = -70
+    robot.m2.goal_position = 15.5
+    robot.m3.goal_position = 23.3
+    print("Alert");
 
-time.sleep(3)
 
-robot.m1.goal_position = 70
-robot.m2.goal_position = 50
-robot.m3.goal_position = 62
-print("Up")
-
-time.sleep(2)
-
-printer.println('Hello')
-printer.feed(10)
-
-time.sleep(5)
-
-robot.m1.goal_position = 0
-robot.m2.goal_position = 20.3
-robot.m3.goal_position = -6
-print("Rest")
-
-time.sleep(3)
+def up(z, x, c):
+    robot.m1.moving_speed = z
+    robot.m2.moving_speed = x
+    robot.m3.moving_speed = c
+    robot.m1.goal_position = 70
+    robot.m2.goal_position = 50
+    robot.m3.goal_position = 62
+    print("Up")
