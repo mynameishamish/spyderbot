ffab2f2658af7b71de05ccdb783d466bd4b66f0f
mynameishamish <hamish.tennent@gmail.com>
2017-12-06 19:49:31 -0500

    Tidying

--- deprec/spidernew/pypot/readtest.py
+++
@@ -1,6 +0,0 @@
-import pypot.robot
-
-spider_robot = pypot.robot.from_config(spider_robot_config)
-
-for m in spider_robot.base:
-    print(m.present_position)
--- deprec/spidernew/start.py
+++
@@ -1,2 +0,0 @@
-from config import spider_robot_config
-
