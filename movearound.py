899f758a08957ff360311c0d2e1afadddc889c72
imandel <Im334@cornell.edu>
2018-04-15 12:46:37 -0400

    movearound testing

--- movearound.py
+++ movearound.py
@@ -16,22 +16,37 @@ for m in robot.motors: # Note that we always provide an alias for all motors.
     m.compliant = False
     m.set_moving_speed = 200
 
+resting()
+limp()
+livly()
+resting()
+# concur()
 
+
+# print("returning home")
+# easingMultiple(motionrest, 1)
+# time.sleep(2)
+# print("compliant")
+
+# for m in robot.motors: # Note that we always provide an alias for all motors.
+#     m.compliant = True
+#     m.set_moving_speed = 200
+# time.sleep(20)
 #
-print("starting")
-resting(50, 50, 50)
+# print("starting")
+# resting(50, 50, 50)
 
-print("alert")
-easingMultiple(motionalert, 1)
+# # print("alert")
+# # easingMultiple(motionalert, 1)
 
 
-print("forward")
-easingMultiple(motionforward, 1)
-time.sleep(1)
+# print("forward")
+# easingMultiple(motionforward, .3)
+# time.sleep(1)
 
-print("returning home")
-easingMultiple(motionrest, 1)
-time.sleep(2)
+# print("returning home")
+# easingMultiple(motionrest, .3)
+# time.sleep(2)
 
 # print("forward")
 # easingMultiple(motionforward, .5)
