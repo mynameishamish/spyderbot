8df031851733e354ced09e47a1a20ba4e640bddb
mynameishamish <hamish.tennent@gmail.com>
2018-04-18 11:08:50 -0400

    Adding Shuran's Code into runmotion.py

---
+++ runmotion.py
@@ -0,0 +1,149 @@
+
+import time
+import math
+import numpy
+
+from easing import *
+from motions import *
+
+#easing 
+x = easeInOutSine
+y = easeOutBack
+linear = linearTween
+s = easeInExpo
+eib = easeInBack
+eoc = easeOutCirc
+
+# motions
+
+print("starting Easepos")
+
+motionrest= [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , x, robot.m2.present_position, -77] ,
+    [robot.m3 , x, robot.m3.present_position, 73]]
+
+motionalert= [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , x, robot.m2.present_position, -65] ,
+    [robot.m3 , x, robot.m3.present_position, 135]]
+
+motionforward= [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , x, robot.m2.present_position, -45] ,
+    [robot.m3 , x, robot.m3.present_position, 149]]
+
+motionoffer= [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , x, robot.m2.present_position, -20] ,
+    [robot.m3 , x, robot.m3.present_position, 112]]
+
+
+motionofferNew= [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , y, robot.m2.present_position, -15] ,
+    [robot.m3 , y, robot.m3.present_position, 110]]
+
+motionNodup = [
+    [robot.m3 , linear, robot.m3.present_position, 120]]
+
+motionNoddown = [
+    [robot.m3 , linear, robot.m3.present_position, 100]]
+
+motionAfteroffer = [
+    # [robot.m1 , x, robot.m1.present_position, robot.m1.present_position] ,
+    [robot.m2 , y, robot.m2.present_position, -60] ,
+    [robot.m3 , y, robot.m3.present_position, 135]]
+
+midpos = [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , x, robot.m2.present_position, -65] ,
+    [robot.m3 , x, robot.m3.present_position, 135]
+]
+
+turnaway = [
+    [robot.m1 , x, robot.m1.present_position, 57.33] ,
+    [robot.m2 , x, robot.m2.present_position, -32.4] ,
+    [robot.m3 , x, robot.m3.present_position, 103.42]
+]
+
+listen = [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , x, robot.m2.present_position, -84.9] ,
+    [robot.m3 , x, robot.m3.present_position, 107.18]
+]
+
+strench  = [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , s, robot.m2.present_position, -57] ,
+    [robot.m3 , x, robot.m3.present_position, 146]
+]
+
+look = [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , eib, robot.m2.present_position, -53] ,
+    [robot.m3 , x, robot.m3.present_position, 143]
+]
+
+read = [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , x, robot.m2.present_position, -27] ,
+    [robot.m3 , x, robot.m3.present_position, 114]
+]
+
+musicdown = [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , eoc, robot.m2.present_position, -65] ,
+    [robot.m3 , x, robot.m3.present_position, 75]
+]
+
+musicup = [
+    [robot.m1 , x, robot.m1.present_position, -4] ,
+    [robot.m2 , eoc, robot.m2.present_position, -55] ,
+    [robot.m3 , x, robot.m3.present_position, 99]
+]
+resting(30,30,30)
+
+time.sleep(2)
+
+#run motions!
+while True: 
+    print('Choose a motion: ')
+    command = input()
+    if command == "offer":
+        print("offer")
+        easingMultiple(motionofferNew, 1.5)
+    elif command == "nod":
+        print("nod")
+        easingMultiple(motionNodup, 1)
+        easingMultiple(motionNoddown, 1)
+    elif command == "midpos":
+        print("midpos")
+        easingMultiple(midpos,1)
+    elif command == "listen":     
+        easingMultiple(listen,1)
+    elif command == "turnaway":
+        easingMultiple(turnaway,1)
+    elif command == "awake":
+        easingMultiple(strench,2)
+        time.sleep(1)
+        easingMultiple(listen,1)
+    elif command == "look":
+        easingMultiple(look,1)
+    elif command == "read":
+        easingMultiple(read, 1)
+    elif command == "dance":
+        while True:
+            easingMultiple(musicdown,1)
+            easingMultiple(musicup,1)
+            time.sleep(0.2)
+            n = input()
+            if n == "break":
+                break
+    elif command == "rest":
+        easingMultiple(motionrest, 1.5)
+    elif command == "break":
+        print("rest")
+        easingMultiple(motionrest, 1.5)
+        break
+
