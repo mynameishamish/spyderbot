60ecab54bc22c9d7e5af6900b4a5ec2788bdcbc9
mynameishamish <hamish.tennent@gmail.com>
2018-06-01 20:17:04 -0400

    starting global move.py file

---
+++ move.py
@@ -0,0 +1,29 @@
+import time
+import math
+import numpy
+import os
+
+from easing import *
+from motions import *
+from Adafruit_Thermal import *
+
+printer = Adafruit_Thermal("/dev/ttyUSB0", 19200, timeout=5)
+
+speed=200
+
+for m in robot.motors:
+    m.compliant = False
+    m.set_moving_speed = 10
+robot.m1.moving_speed = 200
+robot.m2.moving_speed = 40
+robot.m3.moving_speed = 40
+
+print("starting")
+
+run = True
+
+while run:
+
+    overheating()
+
+resting()
