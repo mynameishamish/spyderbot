1e567e7899c615d30ced9a6dcd0fe99b5057e5ae
mynameishamish <hamish.tennent@gmail.com>
2018-06-02 15:01:41 -0400

    passing arguments into move.py from node

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ io.on('connection', function(socket){
 
   socket.on('moveFunction', function(movement){
     console.log("Sending movement code:" + movement);
-    new PythonShell('move.py').send(movement);
+    // new PythonShell('move.py').send(movement);
   });
 
   socket.on('mic', function(){
