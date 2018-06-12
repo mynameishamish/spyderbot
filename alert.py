9b79b537de560ef3c1adf7a22a9f2001f325ee31
mynameishamish <hamish.tennent@gmail.com>
2018-06-12 14:21:34 -0400

    working on moving files

--- alert.py
+++
@@ -1,9 +0,0 @@
-import time
-import math
-import numpy
-
-from easing import *
-from motions import *
-from Adafruit_Thermal import *
-alertmotion()
-time.sleep(2)
---
+++ motionfiles/alert.py
@@ -0,0 +1,9 @@
+import time
+import math
+import numpy
+
+from .. from easing import *
+from .. from motions import *
+from .. from Adafruit_Thermal
+alertmotion()
+time.sleep(2)
