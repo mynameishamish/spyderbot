be8ad6c4c62912b9829f85055ef01184b22d489f
mynameishamish <hamish.tennent@gmail.com>
2018-06-22 19:13:09 -0400

    added pythonshell twice

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.use(express.static('public'));
 //   });
 // });
 
-var PythonShell = require('python-shell');
 var pyshell = new PythonShell('move.py');
 
 pyshell.on('message', function (message) {
