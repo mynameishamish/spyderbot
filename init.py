0e27a82db3c680a2f06f13a3b5d46413eb2d0d15
egendreau <egendreau254@gmail.com>
2018-07-24 16:22:26 -0400

    added going home motion to init()

--- init.py
+++ init.py
@@ -9,3 +9,7 @@ from Adafruit_Thermal import *
 for m in robot.motors: # Note that we always provide an alias for all motors.
     m.compliant = False
     m.set_moving_speed = 200
+
+print("resting")
+easingMultiple(motionrest, .8)
+time.sleep(.5)
