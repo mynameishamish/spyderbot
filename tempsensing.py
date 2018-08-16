f991c4c0619e1a47921bbde6fb63182ad9115ff2
mynameishamish <hamish.tennent@gmail.com>
2018-08-16 11:59:01 -0400

    added tempsending file

--- tempsensing.py
+++ tempsensing.py
@@ -44,23 +44,29 @@ ADAFRUIT_IO_USERNAME = 'mynameishamish'
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 # Set up Adafruit IO Feeds.
-temperature_feed = aio.feeds('spyderbot.head')
-humidity_feed = aio.feeds('spyderbot.neck')
+m1temp = aio.feeds('spyderbot.base')
+m2temp = aio.feeds('spyderbot.head')
+m3temp = aio.feeds('spyderbot.neck')
+
 
 # Set up DHT22 Sensor.
 # dht22_sensor = Adafruit_DHT.DHT22
 
 while True:
     # humidity, temperature = Adafruit_DHT.read_retry(dht22_sensor, DHT_DATA_PIN)
-    humidity = robot.m2.present_temperature
-    temperature = robot.m3.present_temperature
+    m1temp = robot.m1.present_temperature
+    m2temp = robot.m2.present_temperature
+    m3temp = robot.m3.present_temperature
     if humidity is not None and temperature is not None:
-        print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
+        print('m1Temp={0:0.1f}*C m2Temp={1:0.1f}*C m3Temp={1:0.1f}*C'.format(m1temp, m2temp, m3temp))
         # Send humidity and temperature feeds to Adafruit IO
-        temperature = '%.2f'%(temperature)
-        humidity = '%.2f'%(humidity)
-        aio.send(temperature_feed.key, str(temperature))
-        aio.send(humidity_feed.key, str(humidity))
+        m1temp = '%.2f'%(m1temp)
+        m2temp = '%.2f'%(m2temp)
+        m3temp = '%.2f'%(m3temp)
+
+        aio.send(m1temp.key, str(m1temp))
+        aio.send(m2temp.key, str(m2temp))
+        aio.send(m3temp.key, str(m3temp))
     else:
         print('Failed to get DHT22 Reading, trying again in ', DHT_READ_TIMEOUT, 'seconds')
     # Timeout to avoid flooding Adafruit IO
