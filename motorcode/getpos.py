8de1902623731782efcb20fccb569beffcab5b36
mynameishamish <hamish.tennent@gmail.com>
2018-08-16 17:52:03 -0400

    changed declarations in getpos

--- motorcode/getpos.py
+++ motorcode/getpos.py
@@ -2,32 +2,51 @@ spyder_config = {
     'controllers': {
         'my_dxl_controller': {
             'sync_read': False,
-            'attached_motors': ['base'],
+            'attached_motors': ['robot1', 'robot2'],
             'port': 'auto'
         }
     },
     'motorgroups': {
-        'base': ['m1', 'm2', 'm3']
+        'robot1': ['m1', 'm2', 'm3'],
+        'robot2': ['m4', 'm5', 'm6']
     },
     'motors': {
         'm1': {
             'orientation': 'direct',
-            'type': 'AX-12A', 'id': 1,
-            'angle_limit': [-90.0, 90.0],
+            'type': 'AX-12A', 'id': 4,
+            'angle_limit': [-180.0, 180.0],
             'offset': 0.0
         },
         'm2': {
             'orientation': 'direct',
-            'type': 'AX-18A', 'id': 2,
-            'angle_limit': [4.0, 50.0],
+            'type': 'AX-18A', 'id': 5,
+            'angle_limit': [-180.0, 180.0],
             'offset': 0.0
         },
         'm3': {
             'orientation': 'direct',
-            'type': 'AX-18A', 'id': 3,
-            'angle_limit': [0.0, 38.0],
+            'type': 'AX-18A', 'id': 6,
+            'angle_limit': [-180.0, 180.0],
             'offset': 0.0
-        }
+        },
+        'm4': {
+            'orientation': 'direct',
+            'type': 'AX-12A', 'id': 1,
+            'angle_limit': [-180.0, 180.0],
+            'offset': 0.0
+        },
+        'm5': {
+            'orientation': 'direct',
+            'type': 'AX-12A', 'id': 2,
+            'angle_limit': [-180.0, 180.0],
+            'offset': 0.0
+        },
+        'm6': {
+            'orientation': 'direct',
+            'type': 'AX-12A', 'id': 3,
+            'angle_limit': [-180.0, 180.0],
+            'offset': 0.0
+        },
     }
 }
 import time
@@ -40,11 +59,14 @@ freq = 0.5
 
 robot = pypot.robot.from_config(spyder_config)
 
-# Put the robot in its initial position
-for m in robot.motors: # Note that we always provide an alias for all motors.
-    m.compliant = True
+robot.m1.compliant = True
+robot.m2.compliant = True
+robot.m3.compliant = True
+robot.m4.compliant = True
+robot.m5.compliant = True
+robot.m6.compliant = True
 
-    # m.goal_position = 0
+
 
 # Wait for the robot to actually reach the base position.
 
