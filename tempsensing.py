4365aaa62c920dbf117925166310d254247bf084
mynameishamish <hamish.tennent@gmail.com>
2018-08-16 11:49:01 -0400

    added tempsending file

--- tempsensing.py
+++ tempsensing.py
@@ -44,8 +44,8 @@ ADAFRUIT_IO_USERNAME = 'mynameishamish'
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 # Set up Adafruit IO Feeds.
-temperature_feed = aio.feeds('temperature')
-humidity_feed = aio.feeds('humidity')
+temperature_feed = aio.feeds('head')
+humidity_feed = aio.feeds('neck')
 
 # Set up DHT22 Sensor.
 # dht22_sensor = Adafruit_DHT.DHT22
