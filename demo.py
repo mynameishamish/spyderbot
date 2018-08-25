1eda0958e9460aabd8d2a6dfe17dcc8208c4fb81
mynameishamish <hamish.tennent@gmail.com>
2018-08-25 17:20:06 -0400

    started demo file

--- demo.py
+++ demo.py
@@ -6,16 +6,25 @@ from easing import *
 from motions import *
 from Adafruit_Thermal import *
 
+printer = Adafruit_Thermal("/dev/ttyUSB0", 19200, timeout=5)
+
+print(robot.m1.present_position)
+print(robot.m2.present_position)
+print(robot.m3.present_position)
+
+
 resting()
 time.sleep(1)
 alert()
 time.sleep(1)
 forward()
 time.sleep(.5)
-printer.println('hello')
+printer.println("hello")
 time.sleep(.2)
 offer()
-time.sleep(2)
+time.sleep(.5)
+printer.feed(2)
+time.sleep(1.5)
 alert()
 time.sleep(4)
 resting()
