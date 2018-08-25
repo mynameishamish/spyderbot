fc8423368bad39b433b02f009aabf6c082bfe777
mynameishamish <hamish.tennent@gmail.com>
2018-08-25 17:34:44 -0400

    started demo file

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+
 import time
 import math
 import numpy
@@ -6,29 +7,26 @@ from easing import *
 from motions import *
 from Adafruit_Thermal import *
 
+
 printer = Adafruit_Thermal("/dev/ttyUSB0", 19200, timeout=5)
 
-print(robot.m1.present_position)
-print(robot.m2.present_position)
-print(robot.m3.present_position)
+
 
 for m in robot.motors: # Note that we always provide an alias for all motors.
     m.compliant = False
     m.set_moving_speed = 200
 
-easingMultiple(motionrest, 3)
+
+easingMultiple(motionrest, 2)
 time.sleep(1)
+
 easingMultiple(motionalert, 2)
-time.sleep(1)
+time.sleep(2)
+
 easingMultiple(motionforward, 2)
-time.sleep(.5)
-printer.println("hello")
-time.sleep(.2)
+time.sleep(2)
+
 easingMultiple(motionoffer, 2)
-time.sleep(.5)
-printer.feed(4)
-time.sleep(1.5)
-easingMultiple(motionalert, 2.4)
-time.sleep(4)
-easingMultiple(motionrest, 3)
-time.sleep(.5)
+time.sleep(2)
+
+easingMultiple(motionrest, 2)
